27d1c57ebe68487298ccf45f6bb563b877becbac
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-04 17:18:06 -0300

    Importing

---
+++ Suraj Tutorials/FilesManipulation/OutputFile.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+// file stream
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    /**
+    * ====OUTPUT TO FILE====
+    * - IF FILE DOESN'T EXIST WITH A CERTAIN NAME IT WILL BE CREATED
+    * - TEXT OR BINARY
+    * - FILE-TYPE DOESN'T REALLY MATTER, IT WILL BE TEXT ANYWAY
+    * - FLAGS
+    * - BE CAREFUL WITH OPENING FILES AND SAVING STUFF
+    * - BE CAREFUL WITH WHILE LOOPS
+    */
+    
+    string data = "DATA \n New Line 2.";
+    int i = 23;
+
+    string fileName = "File.expf";
+
+    ofstream outFile;
+
+    // Create and Open File
+    outFile.open(fileName);
+
+    if(outFile.is_open()) {
+        // (outFile <<) = Writing data (w)
+        // (outFile >>) = Reading data (r)
+        
+        // Add Data to the file
+        outFile << data << " " << i;
+    }
+    else {
+        cout << "n\n";
+    }
+
+    outFile.close();
+
+    system("PAUSE");
+}
