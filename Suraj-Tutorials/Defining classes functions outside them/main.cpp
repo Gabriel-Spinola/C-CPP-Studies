7c8ede13d00a24ba6086ec6b8f31caf979074c59
gseven-gdv <69523963+gseven-gdv@users.noreply.github.com>
2021-02-27 15:18:54 -0300

    Update main.cpp

--- Suraj-Tutorials/Defining classes functions outside them/main.cpp
+++ Suraj-Tutorials/Defining classes functions outside them/main.cpp
@@ -18,8 +18,6 @@ class MyClass {
 
         // Accessors
         inline const std::vector<std::string>& getMyStrings() const;
-
-
 };
 
 MyClass::MyClass() {
@@ -28,10 +26,34 @@ MyClass::MyClass() {
 
 inline const std::vector<std::string>& MyClass::getMyStrings() const { return this->myStrings; }
 
+void MyClass::add(const std::string str) {
+    this->myStrings.push_back(str);
+}
+
+const std::string MyClass::toString() const {
+    std::string finalString;
+
+    for(int i = 0; i < this->myStrings.size(); i++) {
+        finalString += std::to_string(i) + ": " + this->myStrings[i] + "\n";
+    }
+
+    return finalString;
+}
+
 int main() {
     MyClass instance;
+    
+    std::cout << instance.toString() << std::endl;
+    
+    instance.add("Cool");
 
+    std::cout << instance.toString() << std::endl;
+
+    /*
     for(int i = 0; i < instance.getMyStrings().size(); i++) {
+        // * Both makes the same thing
         std::cout << instance.getMyStrings()[i] << std::endl;
+        std::cout << instance.getMyStrings().at(i) << std::endl;
     }
+    */
 }
