792e6c6087cf37f4cb6c90138b29b757cc952b20
gseven-gdv <zerogravitystd@gmail.com>
2021-03-09 15:33:49 -0300

    Update main.c

--- C/Other/pointers/main.c
+++ C/Other/pointers/main.c
@@ -1,8 +1,9 @@
 int main() {
-    int array[1024] = { 0 }; // set all the 1024 values to 0
+    int array[1024] = { 0 }; // * set all the 1024 values to 0
 
-    int *ptr = array; // create a pointer thar point to the array (it means that this pointers stores the memory addres of this array)
+    int *ptr = array; // * create a pointer thar point to the array (it means that this pointers stores the memory addres of this array)
 
+    // NOTE
     // ptr += 2; this means that you are changing the value that this pointer it's pointing and not it's actual value. Example below
     // where the pointer is pointing is assigned to the letter p
     // this [] means the index of the array
@@ -20,6 +21,7 @@ int main() {
     char *ptr = array;
 
     ptr += 2; 
+    // NOTE
     // Now when you do that, you're not anymore changing what index the pointer is pointing
     // you're changing the byte that this pointer is pointing (int = 4 bytes)
     // from the first byte you go to the third byte
@@ -29,26 +31,26 @@ int main() {
     
     /**
      * ``` C
-     *  int *ptr = NULL; // you cannot deref. NULL
+     *  int *ptr = NULL; // NOTE you cannot deref. NULL
      *  *ptr = 0; // ! crash
      *  ptr[i] = 0; // ! crash
      * ```
      * 
      * ``` C
-     *  Man *man; // you cannot deref. uninitialized 'wild' pointers
+     *  Man *man; // NOTE you cannot deref. uninitialized 'wild' pointers
      *  man->x = 50; // ! crash
      *  man->name = NULL; // ! crash
      * ```
      * 
      * ``` C
-     *  Man men[10] // you cannot deref. outside the bounds od an array
+     *  Man men[10] // NOTE you cannot deref. outside the bounds od an array
      * 
      *  men[500].x = 50; // ! Crash
      *  men[10].y = 50; // ! Crash
      * ```
      * 
      * ``` C
-     *  char *str1 = "a string"; // you cannot modify string constantss
+     *  char *str1 = "a string"; // NOTE you cannot modify string constantss
      *  char str2[1024] = "a string";
      * 
      *  str1[0] = 'b'; // ! Crash
